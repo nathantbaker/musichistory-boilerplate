f6dcba752a6a0774ada4e070d4e2caeeb5228a30
Nathan T. Baker <nathantbaker@gmail.com>
2016-10-20 17:25:22 -0500

    songs get injected into html

--- js/main.js
+++ js/main.js
@@ -1,32 +1,27 @@
-console.log("hello world");
-
 var songs = [
-  "The Nights of Wines and Roses > by Japandroids on the album Celebration Rock",
   "Legs > by Z*ZTop on the album Eliminator",
   "The Logical Song > by Supertr@amp on the album Breakfast in America",
   "Another Brick in the Wall > by Pink Floyd on the album The Wall",
   "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction",
-  "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill",
-  "What Do You Mean? > by Justin Bieber on the album Purpose"
+  "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill"
 ];
 
-// Target where we're injecting songs
+// adding a song to the beggining and end, because requirements
+songs.unshift("The Nights of Wines and Roses > by Japandroids on the album Celebration Rock");
+songs.push("What Do You Mean? > by Justin Bieber on the album Purpose");
+
+for (var i = 0; i < songs.length; i++) {
+  // replace > for - and remove characters that don't belong
+  songs[i] = songs[i].replace(">", "-").replace(/[@*(!]/g, "");
+}
+
+// Target where and what we're injecting
 var html = document.getElementById("songs");
 var htmlBefore = "<article><p>";
 var htmlAfter = "</article></p>";
 
 // Inject songs into HTML
 for (var i = 0; i < songs.length; i++) {
-  var stringToAdd = htmlBefore + "Song " + (i + 1) + htmlAfter;
+  var stringToAdd = htmlBefore + songs[i] + htmlAfter;
   html.innerHTML += stringToAdd;
 }
-
-// Students must use JavaScript to create a list of songs in the `index.html` file for their Music History project.
-
-// 1. Loop over the array and remove any words or characters that obviously don't belong.
-// 1. Students must find and replace the `>` character in each item with a `-` character.
-// 1. Must add each string to the DOM in `index.html` in the main content area.
-
-//  ------------------------------------------------
-// |  {Song name} by {Artist} on the album {Album}  |
-//  ------------------------------------------------
